c51ba1f9f01ab7af08bf7a1788ceb7a81bf5a82c
Nimish Sule <nimishsule97@gmail.com>
2021-12-05 19:31:25 -0500

    Tree http

--- demo/src/app/nodeviz/nodeviz.component.ts
+++ demo/src/app/nodeviz/nodeviz.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core'
+import {HttpClient} from "@angular/common/http";
 
 declare var Treant: any;
 
@@ -11,7 +12,7 @@ declare var Treant: any;
 })
 export class NodevizComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   config = {
     container: "#treant-id"
@@ -23,8 +24,8 @@ export class NodevizComponent implements OnInit {
 
     var current_round = 0;
     var num_of_rounds = Math.ceil(Math.log(nodes_list.length)/Math.log(branching_factor))
-    console.log(nodes)
-    console.log(num_of_rounds)
+    // console.log(nodes)
+    // console.log(num_of_rounds)
     while(num_of_rounds-current_round){
       let cur_mod = Math.pow(branching_factor,num_of_rounds-current_round-1)
       console.log("current_mod" + cur_mod.toString())
@@ -43,16 +44,30 @@ export class NodevizComponent implements OnInit {
       )
       nodes.push(cur_nodes);
       current_round+=1;
-      console.log(nodes)
+      // console.log(nodes)
     }
-    console.log(nodes.flat())
+    // console.log(nodes.flat())
     return nodes.flat();
   };
 
   ngOnInit() {
+    this.http.get('http://localhost:8080/node_list').subscribe((data: Object) => {
+      // console.log("response:");
+      // console.log(data);
+      // var data2 = JSON.parse(data)
+      // a = data.branching
+      // b = data.node_list
+      // var dat = data || Object() ;
+      // dat["branching"] = 4;
+      // dat["nodes_list"] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      (() => {
+        Treant([this.config].concat(this.create_tree_visualization(Object.values(data)[0], Object.values(data)[1])))
+      })();
+    });
+
     // this.create_tree_visualization(2, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    this.create_tree_visualization(4, [0, 1, 2, 3, 4]);
-    (() => {Treant([this.config].concat(this.create_tree_visualization(4, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])))})();
+    // this.create_tree_visualization(4, [0, 1, 2, 3, 4]);
+    // (() => {Treant([this.config].concat(this.create_tree_visualization(4, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])))})();
   }
 
   // ngOnInit(): void {
