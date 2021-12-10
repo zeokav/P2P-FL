e24d3e7706a1abfadc8bd0dff58eff3cc714f5fb
nirajsrimal <nirajsrimal@gmail.com>
2021-12-09 23:45:59 -0500

    Changed epoch size to 5

--- src/p2plib/cmd/flat/fl_peer.py
+++ src/p2plib/cmd/flat/fl_peer.py
@@ -131,7 +131,7 @@ class FLPeer:
                     'model_id': self.model_id,
                     'train_size': 20000,
                     'data_split': (0.6, 0.3, 0.1), # train, test, valid
-                    'epoch_per_round': 1,
+                    'epoch_per_round': 5,
                     'batch_size': 16,
                     'weights': self.global_model.current_weights
                 }
